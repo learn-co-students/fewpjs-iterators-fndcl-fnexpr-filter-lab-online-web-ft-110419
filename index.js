5f457af1e0ba880098228a9d841542844416c9f0
Brandon Weaver <a7xncob@gmail.com>
2020-02-25 21:46:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver[0].toLowerCase() === string[0].toLowerCase();
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.name.toLowerCase() === string.toLowerCase();
+  });
+}
