3cc109507879984ab3c8274db8060a6f02a545e3
jessesbyers <jesse.s.byers@gmail.com>
2020-02-27 17:20:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution here
+function findMatching(array, string) {
+    let names = array.filter(function(n) { 
+         return n === string || n === string.toLowerCase() || n === string.toUpperCase();
+    })
+    return names
+}
