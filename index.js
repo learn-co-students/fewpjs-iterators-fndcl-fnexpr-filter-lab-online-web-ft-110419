85a8514aba2218d51060b2f386c019c0cb2b006f
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-02-28 22:52:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(driversArr, str){
+    // let drivers = driversArr.map(n => n.toLowerCase());
+    let match = driversArr.filter(n => {return n.toLowerCase() == str.toLowerCase();})
+    return match;
+};
+
+function fuzzyMatch(driversArr, str){
+    let letter = str.split('')[0];
+    // let drivers = driversArr.map(n => n.toLowerCase());
+    let match = driversArr.filter(n => {return n.startsWith(letter);})
+    return match
+
+
+}
+
+function matchName(driversArr, str){
+    let match = driversArr.filter(n => {return n.name === str;})
+    return match;
+}
