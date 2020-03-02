601df5e6f51d6195b6ef384091d2b7803e97ece2
Richell Bonfield <richell.bonfield12@gmail.com>
2020-03-02 06:31:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, d) {
+  return drivers.filter( match =>
+    match.toLowerCase() === d.toLowerCase()
+  )
+}
+
+function fuzzyMatch(drivers, s) {
+  return drivers.filter( match =>
+    match.toLowerCase().indexOf(s.toLowerCase()) === 0
+  )
+}
+
+function matchName(drivers, driverName) {
+  return drivers.filter( record => record.name === driverName)
+}
