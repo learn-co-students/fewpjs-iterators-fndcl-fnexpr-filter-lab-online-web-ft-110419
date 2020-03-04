839543fe37d105337e4f59286907970da83e6e93
C-Mike576 <m.calkins576@gmail.com>
2020-03-04 10:18:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(drivers, finder) {
+    let found = drivers.filter(f => {
+        return f.toLowerCase() === finder.toLowerCase()
+    })    
+    return found
+}
+
+function matchName(drivers, finder) {
+    let found = drivers.filter(f => {
+        return f.name === finder
+    })  
+    return found  
+}
+
+function fuzzyMatch(drivers, finder) {
+    let len = finder.length
+    let found = drivers.filter(f => {
+        return f.slice(0, len) === finder 
+    })
+    return found
+}
